8867792c21f4eef8d7ad005aeb6a436bfeca1b6a
WANG XUEYI <47026637+redemptionwxy@users.noreply.github.com>
2021-11-19 21:01:02 +0000

    Update Hello_World.ino

--- Hello_World.ino
+++ Hello_World.ino
@@ -25,7 +25,7 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 void setup() {
   Serial.begin(115200);
   
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) { // Address 0x3D for 128x64
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
